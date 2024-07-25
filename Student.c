07a52d2de4ffa7d836ee3d5722d0431dc877e483
anhtt <anhtt@hsptek.com>
2024-07-25 15:09:31 +0700

    Update

--- Student.c
+++ Student.c
@@ -2,48 +2,24 @@
 #include <string.h>
 #include "Student.h"
 
+void read_string(char *prompt, char *buffer, size_t size) {
+    printf("%s", prompt);
+    if (fgets(buffer, size, stdin) != NULL) {
+        buffer[strcspn(buffer, "\n")] = '\0';
+    }
+}
+
 void input_student_data(Student *student) {
     printf("Nhap thong tin sinh vien:\n");
-  
-    printf("MSSV (8 chu so): ");
-    fgets(student->id, 9, stdin);
-    student->id[strcspn(student->id, "\n")] = '\0'; // Remove newline character
 
-    getchar(); // Remove newline character from input buffer
-
-    printf("Ho ten: ");
-    fgets(student->name, MAX_LEN, stdin);
-    student->name[strcspn(student->name, "\n")] = '\0'; // Remove newline character
-
-    getchar(); // Remove newline character from input buffer
-
-    printf("Ngay sinh: ");
-    fgets(student->dob, MAX_LEN, stdin);
-    student->dob[strcspn(student->dob, "\n")] = '\0'; // Remove newline character
-
-    getchar(); // Remove newline character from input buffer
-
-    printf("Que quan: ");
-    fgets(student->hometown, MAX_LEN, stdin);
-    student->hometown[strcspn(student->hometown, "\n")] = '\0'; // Remove newline character
-
-    getchar(); // Remove newline character from input buffer
-
-    printf("So dien thoai: ");
-    fgets(student->phone, MAX_LEN, stdin);
-    student->phone[strcspn(student->phone, "\n")] = '\0'; // Remove newline character
-
-    getchar(); // Remove newline character from input buffer
-
-    printf("Nganh hoc: ");
-    fgets(student->major, MAX_LEN, stdin);
-    student->major[strcspn(student->major, "\n")] = '\0'; // Remove newline character
-    
-    getchar(); // Remove newline character from input buffer
-
-    printf("Lop: ");
-    fgets(student->class_t, MAX_LEN, stdin);
-    student->class_t[strcspn(student->class_t, "\n")] = '\0'; // Remove newline character
+    read_string("MSSV (8 chu so): ", student->id, 9);
+    getchar();
+    read_string("Ho ten: ", student->name, MAX_LEN);
+    read_string("Ngay sinh: ", student->dob, MAX_LEN);
+    read_string("Que quan: ", student->hometown, MAX_LEN);
+    read_string("So dien thoai: ", student->phone, MAX_LEN);
+    read_string("Nganh hoc: ", student->major, MAX_LEN);
+    read_string("Lop: ", student->class_t, MAX_LEN);
 }
 
 void write_student_to_file(Student *student, const char *filename) {
@@ -72,6 +48,7 @@ void read_and_print_student_data(const char *filename) {
     }
     fclose(file);
 }
+
 void delete_student_data(const char *filename, const char *student_id) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
